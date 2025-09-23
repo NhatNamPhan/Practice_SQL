fb4831b82b48b794f24959c39043b542333c37c7
Nhat Nam <phannam28082004@gmail.com>
2025-09-23 15:31:55 +0700

    finished ex2

--- EX2/ex2.sql
+++ EX2/ex2.sql
@@ -31,15 +31,34 @@ INSERT INTO orders (customer_id, order_date, amount) VALUES
 (4, '2023-02-20', 400),
 (5, '2023-03-01', 350),
 (5, '2023-03-10', 100);
-
-
+SELECT * FROM customers;
+SELECT * FROM orders;
 
 1. Count how many orders each customer placed.
+SELECT customer_id, count(order_id) as total_order_customer
+FROM orders
+GROUP BY customer_id;
 
 2. Show the total sales amount per customer.
+SELECT customer_id, sum(amount) as Total_money
+FROM orders
+GROUP BY customer_id;
 
 3. Find the average order amount for each city.
+SELECT city, avg(amount)
+FROM customers c
+JOIN orders o
+ON c.customer_id = o.customer_id
+GROUP BY city;
 
 4. Show how many orders were placed in each month (by year and month).
+SELECT EXTRACT(MONTH FROM order_date), count (order_id)
+FROM orders o
+JOIN customers c
+ON o.customer_id = c.customer_id
+GROUP BY EXTRACT(MONTH FROM order_date);
 
 5. Find the highest order amount per customer.
+SELECT customer_id, max(amount)
+FROM orders
+GROUP BY customer_id;
