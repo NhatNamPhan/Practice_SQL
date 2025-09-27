7dd80a269eb91c4442dc6482957c1b8d6ba14060
Nhat Nam <phannam28082004@gmail.com>
2025-09-27 17:29:13 +0700

    finished ex5

--- EX5/ex5.sql
+++ EX5/ex5.sql
@@ -18,3 +18,32 @@ INSERT INTO employees_ex5 (first_name, last_name, base_salary, bonus, tax_rate) 
 
 
 SELECT * FROM employees_ex5
+1. Show each employee’s total income (base_salary + bonus).
+SELECT employee_id, 
+		CONCAT(first_name, ' ', last_name) as employee, 
+		(base_salary + bonus) as income
+FROM employees_ex5;
+
+2. Show each employee’s annual salary ((base_salary + bonus) * 12).
+SELECT employee_id, 
+		CONCAT(first_name, ' ', last_name) as employee, 
+		((base_salary + bonus)*12) as annual_income
+FROM employees_ex5;
+
+3. Calculate each employee’s tax amount ((base_salary + bonus) * tax_rate / 100).
+SELECT employee_id, 
+		CONCAT(first_name, ' ', last_name) as employee, 
+		((base_salary + bonus)* tax_rate /100) as tax_amount
+FROM employees_ex5;
+
+4. Show each employee’s salary after tax deduction.
+SELECT employee_id, 
+		CONCAT(first_name, ' ', last_name) as employee, 
+		(base_salary + bonus)- ((base_salary + bonus)* tax_rate /100) as salary_after_tax
+FROM employees_ex5;
+
+5 .Show the remainder when dividing each employee’s annual salary by 1000.
+SELECT employee_id, 
+		CONCAT(first_name, ' ', last_name) as employee, 
+		((base_salary + bonus) * 12) % 1000 as remainder
+FROM employees_ex5;
