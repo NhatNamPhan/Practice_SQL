a1746994d647af2fff15c8d553355db39939d292
Nhat Nam <phannam28082004@gmail.com>
2025-10-12 10:41:31 +0700

    finished ex11

--- EX11/ex11.sql
+++ EX11/ex11.sql
@@ -19,11 +19,27 @@ INSERT INTO employees_ex11 (first_name, last_name, hire_date, birth_date, salary
 SELECT * FROM employees_ex11
 
 1. Show today’s date using CURRENT_DATE.
+SELECT CURRENT_DATE;
+--or 
+SELECT NOW()::date;
 
 2. Extract the year from each employee’s hire date.
+SELECT employee_id, CONCAT(first_name, ' ', last_name), EXTRACT(YEAR FROM hire_date) as year_hire
+FROM employees_ex11;
 
 3. Find employees hired in 2020.
+SELECT *
+FROM employees_ex11
+WHERE EXTRACT(YEAR FROM hire_date) = 2020;
 
 4. Calculate how many years each employee has worked (hint: AGE() or DATE_PART).
+SELECT employee_id, CONCAT(first_name, ' ', last_name), EXTRACT(YEAR FROM AGE(NOW(), hire_date)) as year_worked
+FROM employees_ex11;
+--or
+SELECT employee_id, CONCAT(first_name, ' ', last_name), DATE_PART('year', AGE(NOW(), hire_date)) as year_worked
+FROM employees_ex11; 
 
 5. Show employees who were born in February.
+SELECT *
+FROM employees_ex11
+WHERE EXTRACT(MONTH FROM birth_date) = 2;
